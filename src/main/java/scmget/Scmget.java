89f4e609187b371e2cd45bb0ce7be30436701a83
Giorgos Migdos <cyberpython@gmail.com>
2016-05-25 22:12:46 +0300

    Set the output directory using the Netbeans CVSClient API.
    Remove the restriction that the target dir has to be absent for CVS
    checkouts.
    Print the complete stacktrace to stderr when exceptions occur.

--- src/main/java/scmget/Scmget.java
+++ src/main/java/scmget/Scmget.java
@@ -14,14 +14,13 @@
  */
 package scmget;
 
-import com.beust.jcommander.JCommander;
 import java.io.File;
 import java.io.IOException;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
+
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -35,6 +34,8 @@ import org.netbeans.lib.cvsclient.command.checkout.CheckoutCommand;
 import org.netbeans.lib.cvsclient.connection.AuthenticationException;
 import org.netbeans.lib.cvsclient.connection.PServerConnection;
 import org.netbeans.lib.cvsclient.connection.StandardScrambler;
+
+import com.beust.jcommander.JCommander;
 
 /**
  *
@@ -65,9 +66,6 @@ public class Scmget {
             String targetDir) {
 
         File targetDirectory = new File(targetDir);
-        if (targetDirectory.exists()) {
-            throw new RuntimeException(String.format("CVS checkout - target directory already exists: '%s'", targetDirectory.getAbsolutePath()));
-        }
 
         File targetDirectoryParent = targetDirectory.getParentFile();
         String targetDirParent = targetDirectoryParent.getAbsolutePath();
@@ -93,20 +91,8 @@ public class Scmget {
             try {
                 CheckoutCommand cmd = new CheckoutCommand(true, cvsModule);
                 cmd.setCheckoutByRevision(tagName);
-                boolean result = client.executeCommand(cmd, globalOptions);
-                if (result) {
-                    File checkedOutModuleDir = new File(targetDirectoryParent, FilenameUtils.getName(FilenameUtils.getFullPathNoEndSeparator(FilenameUtils.concat(cvsRoot, cvsModule))));
-                    if (!checkedOutModuleDir.equals(targetDirectory)) {
-                        try {
-                            FileUtils.moveDirectory(checkedOutModuleDir, targetDirectory);
-                            result = true;
-                        } catch (IOException ioe) {
-                            result = false;
-                            throw new RuntimeException(String.format("CVS checkout - failed to move the checked out directory from : %s to %s", checkedOutModuleDir.getAbsolutePath(), targetDirectory.getAbsolutePath()));
-                        }
-                    }
-                }
-                return result;
+                cmd.setCheckoutDirectory(targetDir);
+                return client.executeCommand(cmd, globalOptions);
             } catch (CommandException ce) {
                 ce.printStackTrace();
             }
@@ -181,7 +167,7 @@ public class Scmget {
                     for(ScmCommand scmCmd : xmlConfig.getCommands()){
                         success = scmCmd.apply();
                         if(!success){
-                            System.err.println(gitCmd.getErrorMessage());
+                            System.err.println(scmCmd.getErrorMessage());
                             break;
                         }
                     }
@@ -205,6 +191,7 @@ public class Scmget {
 
             System.exit(success ? 0 : 1);
         }catch(Exception e){
+        	e.printStackTrace();
             System.err.println(e.getMessage());
             System.exit(1);
         }
